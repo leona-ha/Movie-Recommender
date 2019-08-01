a507437666215ecef557fd49e4156de4802d6c6d
Leona Ha <50407361+leona-ha@users.noreply.github.com>
2019-08-01 15:17:23 +0200

    updated recommender

--- recommender/test_recommender.py
+++ recommender/test_recommender.py
@@ -11,3 +11,12 @@ def test_empty_db(client):
     """Start with a blank database."""
     rv = client.get('/')
     assert b'movie' in rv.data
+
+def test_recommender():
+    movie = get_ml_recommendations([[('movie1', 'Titanic'), ('rating1', '4'), ('movie2', 'Shrek'), ('rating2', '4'), ('movie3', 'Toy Story'), ('rating3', '5')])
+    assert type(movie) == str
+
+def test_string_entered():
+    """fails if the user enters a string instead of a number"""
+    with pytest.raises(Exception):
+        get_ml_recommendations([3, "dummy", 5])
